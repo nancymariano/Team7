7a34b0173ed41b98d3111c53618670cb239fbdb1
isabellebutterfield <isabelleb@alleninstitute.org>
2018-02-22 20:23:05 -0800

    added function skeletons from drafted outline

---
+++ name_node.py
@@ -0,0 +1,18 @@
+def list_directory(directory_path):
+    return file_paths
+
+def create_directory(directory_path):
+    return success
+
+def make_file(file_size, file_path):
+    return blocks
+
+def delete_path(path):
+    return success
+
+def read_file(file_path):
+    return blocks
+
+def receive_block_report(data_node_paths):
+    return
+
