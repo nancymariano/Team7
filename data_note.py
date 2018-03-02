0ab843a62eb785a8762a22efdc9bf77423d8e4c5
loleeta <lolitalam3@gmail.com>
2018-03-02 13:08:16 -0800

    add default print msg for datanode fcns

--- data_note.py
+++ data_note.py
@@ -2,33 +2,34 @@
 # Postcondition: file is stored 
 # Function: accepts a data tranasfer from a client
 def acceptWriteFromClient(c, b, path):
-
+    print()
 
 # Precondition: 
 # Postcondition: block report is sent 
 # Function: sends updates on what is being stored on the DataNode
 def sendBlockReport(paths):
-
+    print()
 
 # Precondition: request for a block is received from client
 # Postcondition: block is sent to the client
 # Function: a
 def sendBlockToClient(path): #if client sends a request
-
+    print()
 
 # Precondition: block is received from client
 # Postcondition: block is stored, confirmation is sent to client
 # Function:
 def storeBlock():
-
+    print()
 
 # Precondition: request to delete a block is recvd from client
 # Postcondition: block is removed from storage, and confirmation is sent
 # Function:
 def deleteBlock(path):
-
+    print()
 
 # Precondition: a block is received from another DataNode
 # Postcondition: a block is forwarded to the next DataNode in the path
 # Function:
 def replicateBlock(path):
+    print()
